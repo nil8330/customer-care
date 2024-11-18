bd7c0a1ff0128800015752989cd7a3bdee36dbf3
nil8330 <bekabdbj@gmail.com>
2024-11-18 04:10:37 -0800

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-930ba34.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-03cc8a20' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-bd8c1e3a' );
+});
+ });
